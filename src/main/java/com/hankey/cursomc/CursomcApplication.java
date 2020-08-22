0f6543f87ec03b14fd6b565eca04196af370c3b8
Daniel Guimaraes <dvmguimaraes@gmail.com>
2020-08-22 14:31:10 -0300

    Paginação com parâmetros opcionais na requisição

--- src/main/java/com/hankey/cursomc/CursomcApplication.java
+++ src/main/java/com/hankey/cursomc/CursomcApplication.java
@@ -69,8 +69,16 @@ public class CursomcApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 
+		Categoria cat= new Categoria(null, "Escritório");
 		Categoria cat1 = new Categoria(null, "Informática");
-		Categoria cat2= new Categoria(null, "Escritório");
+		Categoria cat2= new Categoria(null, "Quarto");
+		Categoria cat3 = new Categoria(null, "Banheiro");
+		Categoria cat4= new Categoria(null, "Cozinha");
+		Categoria cat5 = new Categoria(null, "Portaria");
+		Categoria cat6= new Categoria(null, "Lavanderia");
+		Categoria cat7 = new Categoria(null, "Sala de Jogos");
+		Categoria cat8= new Categoria(null, "Varanda");
+		Categoria cat9 = new Categoria(null, "Quarto do bb");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -83,7 +91,7 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
-		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat,cat1,cat2,cat3,cat4,cat5,cat6,cat7,cat8,cat9));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
 		Estado est1 = new Estado(null, "Minas Gerais");
